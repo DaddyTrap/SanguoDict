b34b2ad80022ee2ab8e321c9f7d1f6702423a954
Ken-reddevil <741875304@qq.com>
2017-11-20 21:34:30 +0800

    add edit activity and CircleImageView dependency

---
+++ app/src/main/java/cn/org/sanguodict/sanguodict/EditActivity.java
@@ -0,0 +1,37 @@
+package cn.org.sanguodict.sanguodict;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class EditActivity extends AppCompatActivity {
+
+    private CircleImageView image = null;
+    private EditText name = null;
+    private EditText gender = null;
+    private EditText born = null;
+    private EditText die = null;
+    private EditText native_place = null;
+    private EditText power = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        getViews();
+    }
+
+    private void getViews() {
+        image = (CircleImageView) findViewById(R.id.image);
+        name = (EditText) findViewById(R.id.name_edit);
+        gender = (EditText) findViewById(R.id.gender_edit);
+        born = (EditText) findViewById(R.id.born);
+        die = (EditText) findViewById(R.id.die);
+        native_place = (EditText) findViewById(R.id.native_place_edit);
+        power = (EditText) findViewById(R.id.power_edit);
+    }
+}
